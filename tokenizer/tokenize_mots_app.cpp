f5342b3533c29febd5f732cb37b5e556180b2f2c
Marjorie Armando <marjorie@marjorie-Precision-T1700.(none)>
2018-07-02 12:07:45 +0200

    tokenisation mots de remplacement (reste a tester si j'ai ecrit le bon nb d'expressions de la phrase)

--- tokenizer/tokenize_mots_app.cpp
+++ tokenizer/tokenize_mots_app.cpp
@@ -25,12 +25,13 @@ void reading_lexique(char* lexique_filename, map<string, int>& word_to_id)
 	while(lexique_file >> word && lexique_file >> id)
 		word_to_id[word] = id;
 	word_to_id["-1"] = -1;
-	word_to_id["0"] = 0;
+	//word_to_id["0"] = 0;
 	word_to_id["-2"] = -2;
 	word_to_id["-3"] = -3;
 	word_to_id["ACTUAL"] = -4;
 	word_to_id["NEXT"] = -5;
 	word_to_id["PREV"] = -6;
+	word_to_id["NOWORD"] = 0;
 	cerr << lexique_filename << " has been read" << endl;
 	lexique_file.close();	
 }
@@ -47,26 +48,26 @@ unsigned write_label(string label)
 		return 44;
 }
 
-bool verif(char crochet, unsigned num_word, string& word, char acc, char croch)
+bool verif(char crochet, unsigned num_word, string& word, char acc, char croch, unsigned num_example)
 {
 	bool cro;
 	if(crochet == acc)
 		cro = false;
-	if(crochet == croch)
+	else if(crochet == croch)
 		cro = true;
 	else
 	{
-		cout << "encadrement non valide (ni { ni [) mot numero" << num_word << " : " << word << endl;
+		cout << "encadrement non valide (ni { ni [) mot numero " << num_word << ", dans l'exemple " << num_example << " : " << word << endl;
 		exit(EXIT_FAILURE);
 	}
 	return cro;
 }
 
 
-bool type_word(char c_ouvrant, char c_fermant, unsigned num_word, string& word)
+bool type_word(char c_ouvrant, char c_fermant, unsigned num_word, string& word, unsigned num_example)
 {
-	bool crochet_ouvrant = verif(c_ouvrant,num_word,word,'{','[');
-	bool crochet_fermant = verif(c_fermant,num_word,word,'}',']');
+	bool crochet_ouvrant = verif(c_ouvrant,num_word,word,'{','[',num_example);
+	bool crochet_fermant = verif(c_fermant,num_word,word,'}',']',num_example);
 		
 	if(crochet_ouvrant != crochet_fermant)
 	{
@@ -95,27 +96,42 @@ void tokeniser_nouveau_remplacement(char* lexique, char* file, char* output_f)
 	reading_lexique(lexique, word_to_id);
 	
 	map< string, int >::iterator it;
-	string word, w;
-	unsigned nb_words, num_example=0, nb_words_remplacement;
+	string word, w, word2;
+	unsigned nb_words, num_example=0, nb_words_remplacement, nb_line=0;
 	bool crochet = true;
 	
 	while(f >> word) //lit "-3"
 	{
 		output << word_to_id[word] << endl; //écrit "-3"
+		++nb_line;
 		f >> word; f >> word; //lit "label:" et le label
+		++nb_line;
 		output << write_label(word) << endl; //écrit le label
 		
 		for(unsigned nb_phrase=0; nb_phrase<2; ++nb_phrase)
 		{
 			f >> word; f >> word; //lit "premise" et "-2"
+			++nb_line;
 			f >> nb_words; //nb d'expressions composant la phrase
 			output << nb_words << endl;
+			++nb_line;
 			
 			for(unsigned i=0; i < nb_words; ++i) //pour toutes les expressions composant la phrase
 			{
 				// EXPRESSION DE LA PHRASE
 				f >> word;
-				crochet = type_word(word[0], word[word.size()-1], i,word); //crocher = true si [mot], false si {mot}
+				++nb_line;
+				//cout << "word = " << word << endl;
+				while(word[word.size()-1] != ']' && word[word.size()-1] != '}')
+				{
+					
+					f >> word2;
+					word = word + " ";
+					word = word + word2;
+				}
+				
+				//cout << "EXPRESSION DANS LA PHRASE = " << word << endl;
+				crochet = type_word(word[0], word[word.size()-1], i,word, num_example); //crocher = true si [mot], false si {mot}
 				output << word[0];
 				//extract mots entre crochets
 				for(unsigned i=1; i < word.size()-1; ++i)
@@ -129,7 +145,10 @@ void tokeniser_nouveau_remplacement(char* lexique, char* file, char* output_f)
 					w = ss.str();
 					it = word_to_id.find(w);
 					if( it == word_to_id.end() )
+					{
+						cout << "MOT INCONNU (ligne " << nb_line << ", " << w << ")\n";
 						output << "INCONNU ";
+					}
 					else
 						output << word_to_id[w] << " ";
 				}
@@ -143,6 +162,13 @@ void tokeniser_nouveau_remplacement(char* lexique, char* file, char* output_f)
 					for(unsigned j=0; j < nb_words_remplacement; ++j)
 					{
 						f >> word;
+						while(word[word.size()-1] != '#')
+						{
+							f >> word2;
+							word = word + " ";
+							word = word + word2;
+						}
+						//cout << "EXPRESSION DE REMPLACEMENT = " << word << endl;
 						while(word != "-1")
 						{
 							output << word[0];
@@ -157,14 +183,30 @@ void tokeniser_nouveau_remplacement(char* lexique, char* file, char* output_f)
 								w = ss.str();
 								it = word_to_id.find(w);
 								if( it == word_to_id.end() )
+								{
+									cout << "MOT INCONNU (ligne " << nb_line << ", " << w << ")\n";								
 									output << "INCONNU ";
+								}
 								else
 									output << word_to_id[w] << " ";						
 							}
 							output << word[word.size()-1] << " ";
-							f >> word;
+							f >> word; //lit ACTUAL, NEXT ou PREV
+							//cout << "\tIL EST = " << word << endl;
 							output << word_to_id[word] << " "; 
-							f >> word;
+							f >> word; //lit une expression de remplacement
+							if(word == "-1")
+							{
+								//cout << "\t-1\n";
+								continue;
+							}
+							while(word[word.size()-1] != '#')
+							{
+								f >> word2;
+								word = word + " ";
+								word = word + word2;
+							}
+							//cout << "\tEXPRESSION DE REMPLACEMENT = " << word << endl;
 						}
 						output << "-1 ";
 					}
@@ -175,30 +217,78 @@ void tokeniser_nouveau_remplacement(char* lexique, char* file, char* output_f)
 			}
 		}
 		f >> word; //lit -3
-		output << word_to_id[word] << endl; //écrit -3
+		output << word_to_id[word] << endl << endl << endl; //écrit -3
+		
+		++num_example;
 	}
 	
 	f.close();	
 	output.close();
 }
 
+
+void verif_nb_expressions(char* file)
+{
+	
+	ifstream f(file, ios::in);
+	if(!f)
+	{ 
+		cerr << "Impossible to open the file " << file << endl;
+		exit(EXIT_FAILURE);
+	}	
+	int nb, num=0, nb_words;
+	string word;
+	std::string::size_type sz;
+	while(getline(f,word)) //lit "-3"
+	{	
+		getline(f,word); //lit "label:" et le label
+		getline(f,word);//lit "premise" et "-2"
+		
+		for(unsigned nb_phrase=0; nb_phrase<2; ++nb_phrase)
+		{
+			
+			getline(f,word);
+			nb_words = std::stoi (word,&sz);	//chelou	
+			
+			cout << "NB MOTS = " <<  nb_words << endl;
+			nb=0;
+			getline(f,word);
+			while(word != "-3" && word != "hypothesis -2")
+			{
+				cout << "ligne = " << word << endl;
+				
+				++nb;
+				getline(f,word);
+			}
+			cout << "ligne = " << word << endl;
+			
+			if(nb != nb_words)
+			{
+				cout << "ATTENTION pas le bon nb d'expressions (ecrit " << nb_words << " au lieu de " << nb << ")\n"
+				<< "sample " << num << endl;
+				
+			}
+			
+		}
+		++num;
+		
+	}
+	
+}
+
 int main(int argc, char** argv)
 {
-	if(argc != 8)
+	if(argc != 4)
 	{
 		cerr << "Usage :\n " << argv[0] << "\n\n"
 			 << "lexique\n"
-			 << "file mots remplacant detoken neutral\n"
-			 << "output neutral(file mots remplacant token)\n\n"
-			 << "file mots remplacant detoken inf\n"
-			 << "output inf(file mots remplacant token)\n\n"
-			 << "file mots remplacant detoken contrad\n"
-			 << "output contrad(file mots remplacant token)\n";
+			 << "file mots remplacant\n"
+			 << "output (mots remplacant tokenisé)\n";
 		exit(EXIT_FAILURE);
 	}
 	
 
-	
-	tokeniser_nouveau_remplacement(argv[1], argv[2], argv[3]);
+	verif_nb_expressions(argv[2]);
+	//tokeniser_nouveau_remplacement(argv[1], argv[2], argv[3]);
 	return 0;
 }
