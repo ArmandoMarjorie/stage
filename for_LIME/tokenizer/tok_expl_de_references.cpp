a8b43db7066630d524e28971be77688c978d6ee4
Marjorie Armando <marjorie@marjorie-Precision-T1700.(none)>
2018-07-12 15:14:32 +0200

    expl de reference remis a jour

--- for_LIME/tokenizer/tok_expl_de_references.cpp
+++ for_LIME/tokenizer/tok_expl_de_references.cpp
@@ -98,7 +98,7 @@ void acc(char* expl_ref_filename, char* expl_filename )
 	while(getline(expl_ref,word)) //lit l'id
 	{	
 		cout << "SAMPLE" << word << endl;
-		if(word == "#END#")
+		if(word == " ")
 			break;
 		
 		vector<string> mots_expl_ref;
